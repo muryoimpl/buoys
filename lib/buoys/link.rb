edbf943fb2206b1d5d6ec7bd5e1f971c594c5199
muryoimpl <muryoimpl@gmail.com>
2024-02-25 19:57:05 +0900

    rubocop

--- lib/buoys/link.rb
+++ lib/buoys/link.rb
@@ -6,8 +6,8 @@ module Buoys
     attr_reader :current
 
     CONFIG = {
-      current_class: (Buoys::Config.current_class || 'active'),
-      link_current: (Buoys::Config.link_current || false)
+      current_class: Buoys::Config.current_class || 'active',
+      link_current: Buoys::Config.link_current || false
     }.with_indifferent_access
 
     def initialize(text, url, options)
